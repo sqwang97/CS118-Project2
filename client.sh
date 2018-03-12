085bb5c6267c7f74b2856a91a65944783e65a26f
Shiqi Wang <CeciliaWang@CeliaWangs-MacBook-Pro.local>
2018-03-11 22:40:03 -0700

    client buffer test

--- client.sh
+++ client.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 counter=1
-until [ $counter -gt 50 ]
+until [ $counter -gt 20 ]
 do
 	echo $counter
-	./client localhost 3000 server.cpp 3000 >> result_c.txt
+	echo $counter >> result_c.txt
+	./client_timer localhost 3000 test.txt >> result_c.txt
+
+	echo $counter >> diff_file.txt
+	diff received.data test.txt >> diff_file.txt
 	sleep 1
 	((counter++))
 done
--- server.sh
+++ server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 counter=1
-until [ $counter -gt 50 ]
+until [ $counter -gt 20 ]
 do
 	echo $counter
-	./server 3000 >> result_s.txt
+	echo $counter >> result_s.txt
+	./server_timer 3000 >> result_s.txt
 	((counter++))
 done
